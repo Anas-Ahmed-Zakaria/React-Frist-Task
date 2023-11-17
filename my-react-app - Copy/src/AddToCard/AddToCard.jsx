20a158331e5131e8d11be996aacee3e76f551d85
unknown <anoosahmed567@gmail.com>
2023-11-17 18:22:42 +0200

    done

---
+++ my-react-app - Copy/src/AddToCard/AddToCard.jsx
@@ -0,0 +1,10 @@
+import './AddToCard.css'
+import 'bootstrap'
+import 'react-bootstrap'
+import { Button } from 'react-bootstrap'
+
+export default function AddToCard() {
+  return (
+    <Button className="btn rounded text-warning border-warning w-100 fw-bold">Add To Cart</Button>
+  )
+}
